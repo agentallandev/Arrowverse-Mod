10078ca64ba14548ede4e246f71d19c2f8f10caa
allanep9 <allanep9@gmail.com>
2020-02-02 16:09:09 -0500

    added new items and recipies

---
+++ src/main/java/net/mcreator/arrowverse/MCreatorMaroonOverlay.java
@@ -0,0 +1,44 @@
+package net.mcreator.arrowverse;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+@Elementsarrowverse.ModElement.Tag
+public class MCreatorMaroonOverlay extends Elementsarrowverse.ModElement {
+	@ObjectHolder("arrowverse:maroonoverlay")
+	public static final Item block = null;
+
+	public MCreatorMaroonOverlay(Elementsarrowverse instance) {
+		super(instance, 70);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(MCreatorArrowverseCoreTab.tab).maxStackSize(64));
+			setRegistryName("maroonoverlay");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
